0a3dc4ec35a944791e4f202451459109a2195b43
KIM DOEHEE <kimamy20@gmail.com>
2024-10-11 17:11:49 +0900

    [add] : index.ts export 추가

--- apps/www/src/invitations/dashboard/components/index.ts
+++ apps/www/src/invitations/dashboard/components/index.ts
@@ -1,2 +1,3 @@
 export { default as DashboardHeader } from './DashboardHeader';
 export { default as DashboardTemplate } from './DashboardTemplate';
+export { default as WidgetEditor } from './WidgetEditor';
